52540570a1f6c8143f8051b5a15e1a8be261dccd
abobakaraslam <164588250+abobakaraslam@users.noreply.github.com>
2025-10-21 00:31:01 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+//File: index.js in root directory
+import express from "express"
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.listen(port)
